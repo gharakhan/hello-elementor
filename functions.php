99fac99839313db6de1b2ee3238df258afd16a7d
Ehsan Gharakhan <73024630+gharakhan@users.noreply.github.com>
2024-03-28 11:20:55 +0330

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Theme functions and definitions
+ *
+ * @package HelloElementorChild
+ */
+
+/**
+ * Load child theme css and optional scripts
+ *
+ * @return void
+ */
+function hello_elementor_child_enqueue_scripts() {
+	wp_enqueue_style(
+		'hello-elementor-child-style',
+		get_stylesheet_directory_uri() . '/style.css',
+		[
+			'hello-elementor-theme-style',
+		],
+		'1.0.0'
+	);
+}
+add_action( 'wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts', 20 );
+add_filter( 'auto_update_plugin', '__return_false' );
+
+/**
+ * Add <meta name="keywords" content="focus keywords">.
+ */
+add_filter( 'rank_math/frontend/show_keywords', '__return_true');
